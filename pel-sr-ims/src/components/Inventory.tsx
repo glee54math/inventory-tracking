2b9e185149a3086cbd6add1b47756890d9fb3600
glee0695 <1995glee@gmail.com>
2025-07-07 21:46:23 -0700

    Removed things related to submitted Actions.

--- pel-sr-ims/src/components/Inventory.tsx
+++ pel-sr-ims/src/components/Inventory.tsx
@@ -13,70 +13,56 @@ function Inventory({ data }: InventoryProps) {
   const levels = Object.keys(data);
   const subsections: Subsection[] = Object.values(data)[0];
 
- 
   // [ array of Actions ]
   // { subject, level, selectedSubsections[], movementMap, movementNumOfCopiesMap }
 
   return (
     <div>
       <div className="mb-4">
-      <table className="p-2">
-        <thead>
-          <tr>
-            <th className="border border-gray-400 px-2 py-1 text-left">
-              Level
-            </th>
-            {subsections.map((section: Subsection) => (
-              <th
-                key={section.range}
-                className="border border-gray-400 px-2 py-1 text-left relative h-20 w-16"
-              >
-                <div className="absolute inset-0 flex items-center justify-center">
-                  <span className="transform -rotate-45 whitespace-nowrap text-sm">
-                     {section.range} 
-                  </span>
-                </div>
-                
+        <table className="p-2">
+          <thead>
+            <tr>
+              <th className="border border-gray-400 px-2 py-1 text-left">
+                Level
               </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {levels.sort().map((level: string) => (
-            <tr
-              key={level}
-              className="border border-gray-400 px-2 py-1 text-center"
-            >
-              <td>{level}</td>
-              {data[level].map(({ range, count }: Subsection) => (
-                <td
-                  key={range}
-                  className={
-                    count <= 1
-                      ? "border border-gray-400 px-2 py-1 text-center bg-red-200"
-                      : "border border-gray-400 px-2 py-1 text-center"
-                  }
+              {subsections.map((section: Subsection) => (
+                <th
+                  key={section.range}
+                  className="border border-gray-400 px-2 py-1 text-left relative h-20 w-16"
                 >
-                  {count}
-                </td>
+                  <div className="absolute inset-0 flex items-center justify-center">
+                    <span className="transform -rotate-45 whitespace-nowrap text-sm">
+                      {section.range}
+                    </span>
+                  </div>
+                </th>
               ))}
             </tr>
-          ))}
-        </tbody>
-      </table>
-       </div>
-        
-      {submittedActions.map((action, index) => (
-        <pre key={index}>
-          {action.subject}: {action.level}, {action.selectedSubsections}
-          {action.movementMap[action.selectedSubsections[0]]}
-          {action.movementNumOfCopiesMap[action.selectedSubsections[0]]}
-        </pre>
-      ))}
-      {/* {level}
-          {selectedSubsections}
-          {movementMap[selectedSubsections[0]]}
-          {movementNumOfCopiesMap[selectedSubsections[0]]} */}
+          </thead>
+          <tbody>
+            {levels.sort().map((level: string) => (
+              <tr
+                key={level}
+                className="border border-gray-400 px-2 py-1 text-center"
+              >
+                <td>{level}</td>
+                {data[level].map(({ range, count }: Subsection) => (
+                  <td
+                    key={range}
+                    className={
+                      count <= 1
+                        ? "border border-gray-400 px-2 py-1 text-center bg-red-200"
+                        : "border border-gray-400 px-2 py-1 text-center"
+                    }
+                  >
+                    {count}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
