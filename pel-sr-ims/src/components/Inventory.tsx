6a6977dad5c2a24e5458e67753bb98aa2b886a5c
adityatb3 <adityatb0301@gmail.com>
2025-07-15 21:36:04 -0700

    Improved color display for inventory tables

--- pel-sr-ims/src/components/Inventory.tsx
+++ pel-sr-ims/src/components/Inventory.tsx
@@ -1,11 +1,9 @@
-import { useState } from "react";
+// import { useState } from "react";
 // import { useActionContext } from "./ActionContext";
 import type { Subsection, InventoryData } from "../utils/types";
 
 interface InventoryProps {
   data: InventoryData;
-  // location: "Front" | "Back";
-  // subject: "Math" | "English";
 }
 
 function Inventory({ data }: InventoryProps) {
@@ -13,70 +11,55 @@ function Inventory({ data }: InventoryProps) {
   const levels = Object.keys(data);
   const subsections: Subsection[] = Object.values(data)[0];
 
- 
-  // [ array of Actions ]
-  // { subject, level, selectedSubsections[], movementMap, movementNumOfCopiesMap }
-
   return (
     <div>
       <div className="mb-4">
-      <table className="p-2">
-        <thead>
-          <tr>
-            <th className="border border-gray-400 px-2 py-1 text-left">
-              Level
-            </th>
-            {subsections.map((section: Subsection) => (
-              <th
-                key={section.range}
-                className="border border-gray-400 px-2 py-1 text-left relative h-20 w-16"
-              >
-                <div className="absolute inset-0 flex items-center justify-center">
-                  <span className="transform -rotate-45 whitespace-nowrap text-sm">
-                     {section.range} 
-                  </span>
-                </div>
-                
+        <table className="p-2">
+          <thead>
+            <tr>
+              <th className="border border-gray-400 px-2 py-1 text-left">
+                Level
               </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {levels.sort().map((level: string) => (
-            <tr
-              key={level}
-              className="border border-gray-400 px-2 py-1 text-center"
-            >
-              <td>{level}</td>
-              {data[level].map(({ range, count }: Subsection) => (
-                <td
-                  key={range}
-                  className={
-                    count <= 1
-                      ? "border border-gray-400 px-2 py-1 text-center bg-red-200"
-                      : "border border-gray-400 px-2 py-1 text-center"
-                  }
+              {subsections.map((section: Subsection) => (
+                <th
+                  key={section.range}
+                  className="border border-gray-400 px-2 py-1 text-left relative h-20 w-16"
                 >
-                  {count}
-                </td>
+                  <div className="absolute inset-0 flex items-center justify-center">
+                    <span className="transform -rotate-45 whitespace-nowrap text-sm">
+                      {section.range}
+                    </span>
+                  </div>
+                </th>
               ))}
             </tr>
-          ))}
-        </tbody>
-      </table>
-       </div>
-        
-      {submittedActions.map((action, index) => (
-        <pre key={index}>
-          {action.subject}: {action.level}, {action.selectedSubsections}
-          {action.movementMap[action.selectedSubsections[0]]}
-          {action.movementNumOfCopiesMap[action.selectedSubsections[0]]}
-        </pre>
-      ))}
-      {/* {level}
-          {selectedSubsections}
-          {movementMap[selectedSubsections[0]]}
-          {movementNumOfCopiesMap[selectedSubsections[0]]} */}
+          </thead>
+          <tbody>
+            {levels.sort().map((level: string) => (
+              <tr
+                key={level}
+                className="border border-gray-400 px-2 py-1 text-center"
+              >
+                <td>{level}</td>
+                {data[level].map(({ range, count }: Subsection) => (
+                  <td
+                    key={range}
+                    className={
+                      count === 0
+                        ? "border border-gray-400 px-2 py-1 text-center bg-red-200"
+                        : count < 4
+                        ? "border border-gray-400 px-2 py-1 text-center bg-yellow-200"
+                        : "border border-gray-400 px-2 py-1 text-center bg-green-200"    
+                    }
+                  >
+                    {count}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
