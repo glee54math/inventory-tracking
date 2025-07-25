67bce57e207535269a1e4744e536d6ba8675096e
adityatb3 <adityatb0301@gmail.com>
2025-07-25 13:31:49 -0700

    Update sidebar functionality

--- pel-sr-ims/src/components/Sidebar.tsx
+++ pel-sr-ims/src/components/Sidebar.tsx
@@ -1,6 +1,11 @@
 import { useState } from "react";
 
-export default function Sidebar() {
+interface SideBarProps {
+    showInventory: boolean;
+    toggleInventory: () => void;
+}
+
+export default function Sidebar({showInventory, toggleInventory}: SideBarProps) {
     // const [showSidebar, setShowSidebar] = useState<boolean>(false);
 
     const handleSidebar = () => {
@@ -13,9 +18,12 @@ export default function Sidebar() {
         >
             <button
                 id="Inventory"
+                onClick={toggleInventory}
                 className="mt-2 py-2 hover:!bg-green-300 hover:!border-blue-300"
+                title={showInventory ? "Hide" : "Show"}
             >
-                I
+                {showInventory ? "Hide" : "Show"}
+                {/* {showInventory ? "⮜ " : "⮞ "} */}
             </button>
         </div>
     )
