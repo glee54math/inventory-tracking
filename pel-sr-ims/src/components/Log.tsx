b42c73f221d0687840a40db971691f8657e870c4
glee0695 <1995glee@gmail.com>
2025-08-02 00:12:16 -0700

    Log only pulls 20 requests at a time instead of loading ALL the logs.

--- pel-sr-ims/src/components/Log.tsx
+++ pel-sr-ims/src/components/Log.tsx
@@ -1,16 +1,23 @@
 import { useEffect, useState } from "react";
-import { loadLog } from "../utils/inventoryService";
+// import { loadLog } from "../utils/inventoryService";
 import type { LogEntry } from "../utils/types";
-import { collection, onSnapshot } from "firebase/firestore";
+import { collection, getDocs, query, orderBy, limit } from "firebase/firestore";
 import { db } from "../utils/firebase";
 
 function Log() {
   const [actionLog, setActionLog] = useState<LogEntry[]>([]);
   const [visibleCount, setVisibleCount] = useState(20);
-  const visibleLogs = actionLog.slice(0, visibleCount);
+  // const visibleLogs = actionLog.slice(0, visibleCount);
 
   useEffect(() => {
-    const unsubscribe = onSnapshot(collection(db, "logs"), (snapshot) => {
+    const fetchLogs = async () => {
+      const logsQuery = query(
+        collection(db,"logs"),
+        orderBy("timeStamp", "desc"),
+        limit(visibleCount)
+      );
+
+      const snapshot = await getDocs(logsQuery);
       const newLogs: LogEntry[] = [];
 
       snapshot.forEach((doc) => {
@@ -24,36 +31,23 @@ function Log() {
         newLogs.push(logEntry);
       });
       // Sort newest first (optional)
-      newLogs.sort((a, b) => b.timeStamp.getTime() - a.timeStamp.getTime());
+      // newLogs.sort((a, b) => b.timeStamp.getTime() - a.timeStamp.getTime());
       setActionLog(newLogs);
-    });
-    return () => unsubscribe();
-    // const load = async () => {
-    //   const logsFromDB = await loadLog();
-
-    //   // Convert Firestore.Timestamp to JS Date
-    //   const processed = Object.values(logsFromDB).map((entry) => ({
-    //     ...entry,
-    //     timeStamp: entry.timeStamp.toDate() ?? entry.timeStamp, // fallback if already Date
-    //   }));
-
-    //   setActionLog(processed);
-    // };
-    // load();
-  }, []);
+    };
+    
+    fetchLogs();
+  }, [visibleCount]);
 
   return (
     <div className="overflow-auto">
-      {visibleLogs.map((entry, index) => (
+      {actionLog.map((entry, index) => (
         <p key={entry.eventType + index}>
-          {index + 1 + ") " + entry.timeStamp + " | " + entry.message}
+          {index + 1 + ")   " + entry.timeStamp.toLocaleDateString() + " " + entry.timeStamp.toLocaleTimeString() + "  |  " + entry.message}
         </p>
-
-        
       ))}
 
-      {visibleCount < actionLog.length && (
-         <button
+      {(
+        <button
           onClick={() => setVisibleCount((prev) => prev + 20)}
           className="mt-2 px-4 py-1 text-black rounded"
         >
